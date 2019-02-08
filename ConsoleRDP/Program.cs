360bb9f38596fb6ca09d606258a31fad6e3ac5fe
Eujine <sponsor85@mail.ru>
2019-02-08 03:01:46 +0200

    init commit

---
+++ ConsoleRDP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AxMSTSCLib;
+using MSTSCLib;
+
+namespace ConsoleRDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsRdpClient7 rdp = new MsRdpClient7() { Server = "192.168.2.109", UserName = "user" };
+            rdp.AdvancedSettings8.ClearTextPassword = "user";
+            rdp.Connect();
+            Console.WriteLine(rdp.Connected);
+            Console.ReadLine();
+        }
+    }
+}
